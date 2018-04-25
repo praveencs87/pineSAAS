fb8efff1ae43676aede0ecc178f8e52ce92b5878
praveen <praveencs87@gmail.com>
2018-04-25 13:01:57 +0530

    add laravel-cors

--- config/app.php
+++ config/app.php
@@ -215,6 +215,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 		'API' => Dingo\Api\Facade\API::class,
+		Barryvdh\Cors\ServiceProvider::class,
+
 
 
     ],
